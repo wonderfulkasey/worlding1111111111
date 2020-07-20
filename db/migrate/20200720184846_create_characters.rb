7aa0bcfe0d806e0817f2cdcdb517d0214edcad64
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 14:58:30 -0400

    added character migration

---
+++ db/migrate/20200720184846_create_characters.rb
@@ -0,0 +1,14 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :alignment
+      t.string :species
+      t.string :character_class
+
+      t.integer :world_id
+
+      t.timestamps
+    end
+  end
+end
