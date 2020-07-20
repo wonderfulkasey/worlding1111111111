0654205b0eeee4e97e2a2564cd32e1a9ec59a15b
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 15:34:10 -0400

    added resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :worlds
+  resources :characters
  
   root 'world#home'
 
   devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: "callbacks"}
+ 
   devise_scope :user do 
       get 'login', to: 'devise/sessions#new'
   end
