b81cac821c894eb0c5012ceb9a6f785f64c1882c
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 17:25:12 -0400

    took files out of main file

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-
-    def show
-        @user = User.find_by(id :params[:id])
-        @worlds = @user.worlds.reverse
-        @characters = @user.characters.reverse
-    end
-
-end 
