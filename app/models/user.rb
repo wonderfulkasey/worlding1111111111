2c1a729329d827e46268955eefb5341baa1ce2a9
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 16:28:27 -0400

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+
+  has_many :worlds
+  has_many :characters, through: :worlds
+
+  validates :email, uniqueness: true
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
