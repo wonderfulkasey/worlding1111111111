4110f2bb644d8f43234c6d91f0fa0515a370bf5f
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 16:18:35 -0400

    added more routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,19 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: "callbacks"}
+  devise_for :users, controllers: {registrations: "registrations", 
+          omniauth_callbacks: "callbacks"}
  
-  devise_scope :user do 
-      get 'login', to: 'devise/sessions#new'
-  end
-  devise_scope :user do
-    get 'signup', to: 'devise/registrations#new'
-  end
+          devise_scope :user do 
+            get 'login', to: 'devise/sessions#new'
+        end
+      
+        devise_scope :user do 
+          get 'logout', to: 'devise/sessions#destroy'
+        end
+      
+        devise_scope :user do
+          get 'signup', to: 'devise/registrations#new'
+        end
 
 end
