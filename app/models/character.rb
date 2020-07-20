56b81a214a69907170babe75404ff8ad066bba49
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 16:29:34 -0400

    added character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,6 @@
 class Character < ApplicationRecord
+    belongs_to :world
+    belongs_to :user, through: :world
+
+    validates :name, length: { minimum: 2 }
 end
--- app/models/world.rb
+++ app/models/world.rb
@@ -1,5 +1,6 @@
 class World < ApplicationRecord
     belongs_to :user
+    has_many :characters
 
     validates :name, presence: true, length: { minimum: 2 }
 
