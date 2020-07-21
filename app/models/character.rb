5b41bfc8eb5d0a7095390d830e7643b26cf1ccf1
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 20:20:37 -0400

    moved controllers around

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,7 @@ class Character < ApplicationRecord
     belongs_to :user
 
     validates :name, presence: true, length: { minimum: 2 }
+    validates :alignment, presence: true, length: { minimum: 2 }
+    validates :species, presence: true, length: { minimum: 2 }
+    validates :character_class, presence: true, length: { minimum: 2 }
 end
--- app/models/world.rb
+++ app/models/world.rb
@@ -3,7 +3,13 @@ class World < ApplicationRecord
     has_many :characters
 
     validates :name, presence: true, length: { minimum: 2 }
+    validates :aesthetic, presence: true, length: { minimum: 2 }
+    validates :big_bad, presence: true, length: { minimum: 2 }
+    validates :description, presence: true, length: { minimum: 2 }
 
 
+    def self.search(query)
+        where("name LIKE ?", "%#{query}%")
+    end
     #defabc or search?
 end
