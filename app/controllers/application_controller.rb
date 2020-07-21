e920c7eb68d962cd5b29642cbc574ccc830e8af3
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 20:03:37 -0400

    changed controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
 
-    before_action :authenticate_user!
+    helper_method :set_character, :set_world
 
+    def home
+        if params[:query]
+            @characters = Character.search(params[:query])
+            @worlds = World.search(params[:query])
+        end
+    end
+
+    def set_character
+        @character = Character.find(params[:id])
+    end
+
+    def set_world
+        @world = World.find(params[:id])
+
+    end
 end
