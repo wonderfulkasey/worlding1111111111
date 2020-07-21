aa0b9e06f68a618f4b918294f8e97a068b261a3f
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-20 20:53:52 -0400

    deleted user controller

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-
-    def show
-    @user = User.find_by(id: params[:id])
-    @worlds = @user.worlds
-    @characters = @user.characters
-    end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
-  resources :users, only: [:show]
+  #resources :users, only: [:show]
   resources :characters
   resources :worlds do
     resources :characters, only: [:show, :new]
